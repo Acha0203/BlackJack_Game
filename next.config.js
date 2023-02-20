fed861293e7639e55e9dc6c67b9f2796343e4a49
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-02-21 05:24:42 +0900

    chore: modify the top page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    optimizeFonts: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+const Document = () => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Lora:wght@400;600&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+export default Document;
