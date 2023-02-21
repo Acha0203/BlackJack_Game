6d7ff7393cbf42b0a12b6e3d91b08da9ef51ef76
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-02-21 23:02:02 +0900

    feat: modify the name form

--- src/components/ui/InputText.tsx
+++ src/components/ui/InputText.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const InputText = () => {
   const [userName, setUserName] = useState('User');
+
+  useEffect(() => {
+    if (userName === '') {
+      setUserName('User');
+    }
+    console.log(userName);
+  }, [userName]);
 
   return (
     <input
       className='shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
       id='name'
       type='text'
+      onChange={(event) => setUserName(event.target.value)}
       placeholder='Input Your Name'
     />
   );
