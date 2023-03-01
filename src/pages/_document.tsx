444b57fc6c3d62f3bd4271a436d1c3cb84de9e10
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-03-01 16:22:28 +0900

    fix: fix link errors on favicon files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,30 @@ const Document = () => {
   return (
     <Html lang='en'>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-        <link rel='manifest' href='/site.webmanifest' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <link rel='icon' href='https://acha0203.github.io/BlackJack_Game/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='https://acha0203.github.io/BlackJack_Gamehttps://acha0203.github.io/BlackJack_Game/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='https://acha0203.github.io/BlackJack_Gamehttps://acha0203.github.io/BlackJack_Game/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='https://acha0203.github.io/BlackJack_Gamehttps://acha0203.github.io/BlackJack_Game/favicon-16x16.png'
+        />
+        <link rel='manifest' href='https://acha0203.github.io/BlackJack_Game/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='https://acha0203.github.io/BlackJack_Game/safari-pinned-tab.svg'
+          color='#5bbad5'
+        />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff' />
       </Head>
