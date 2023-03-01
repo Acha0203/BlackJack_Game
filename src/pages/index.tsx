eb5a8ca71aa6cbd78c7f6e08f030573b9da27ed6
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-03-01 15:28:38 +0900

    fix: fix the title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import styles from '../styles/Home.module.scss';
@@ -23,26 +24,43 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.curtain_open}>
-      <div className='flex justify-center items-center'>
-        <form className='bg-white shadow-md rounded px-8 pt-6 pb-6'>
-          <div className={styles.cards_icon}>
-            <div className='pt-14'>
-              <TitleSVG />
+    <>
+      <Head>
+        <title>Let&apos;s Play Card Games!</title>
+        <meta
+          name='description'
+          content='You can play the Blackjack card game on this page.'
+          key='desc'
+        />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
+      <div className={styles.curtain_open}>
+        <div className='flex justify-center items-center'>
+          <form className='bg-white shadow-md rounded px-8 pt-6 pb-6'>
+            <div className={styles.cards_icon}>
+              <div className='pt-14'>
+                <TitleSVG />
+              </div>
             </div>
-          </div>
-          <div className='mb-4 w-64'>
-            <InputText />
-          </div>
-          <div className='inline-block relative w-64 mb-4'>
-            <SelectGameType />
-          </div>
-          <div className='flex items-center justify-center'>
-            <StartButton onClick={handleClick} />
-          </div>
-        </form>
+            <div className='mb-4 w-64'>
+              <InputText />
+            </div>
+            <div className='inline-block relative w-64 mb-4'>
+              <SelectGameType />
+            </div>
+            <div className='flex items-center justify-center'>
+              <StartButton onClick={handleClick} />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
